509dc941912ecf7fe4cbb5fe529253950d70bd03
wapdk.09 <wapdk.09@gmail.com>
2023-12-10 19:17:37 +0530

    admin created

---
+++ components/admin/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+  SettingOutlined,
+  DashboardOutlined,
+  MessageOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, theme, Badge, Avatar } from "antd";
+import Link from "next/link";
+
+const { Header, Sider, Content } = Layout;
+
+const Admin = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className="h-screen"
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <DashboardOutlined />,
+              label: <Link href="/admin">Dashboard</Link>,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+          className="flex justify-between p-20"
+        >
+          <div className="flex ">
+            <Button
+              type="text"
+              icon={
+                collapsed ? <MenuUnfoldOutlined className="text-[black]" /> : 
+                <MenuFoldOutlined className="text-[black]"/>
+              }
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+            <h1> Dashboard</h1>
+          </div>
+
+          <div className="w-[200px] flex justify-between items-center mr-10">
+            <Badge size="small" count={5}>
+              <MessageOutlined className="text-[18px]" />
+            </Badge>
+            <Badge size="small" count={1} className="text-[18px]">
+              <NotificationOutlined />
+            </Badge>
+            <Badge size="small">
+              <SettingOutlined className="text-[18px]" />
+            </Badge>
+            <Badge size="small">
+              <Avatar size={"small"} icon={<UserOutlined />} />
+            </Badge>
+          </div>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <div>{children} </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default Admin;
